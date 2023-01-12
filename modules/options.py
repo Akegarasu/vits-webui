abdbc8b340b07cda9237a1b05196a6b58d0e6ee5
Akiba <akiba@anzu.link>
2023-01-12 15:49:38 +0800

    feat: cmd opts

---
+++ modules/options.py
@@ -0,0 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--listen", action='store_true', help="launch gradio with 0.0.0.0 as server name")
+parser.add_argument("--port", type=int, help="launch gradio with given server port, defaults to 8860 if available", default="8860")
+parser.add_argument("--share", action='store_true', help="use share=True for gradio and make the UI accessible through their site")
+parser.add_argument("--server-name", type=str, help="sets hostname of server", default=None)
+parser.add_argument("--autolaunch", action='store_true', help="open the webui URL in the system's default browser upon launch", default=False)
+
+cmd_opts = parser.parse_args()
+
--- webui.py
+++ webui.py
@@ -1,12 +1,15 @@
 import os
+import sys
 from modules.ui import create_ui
 
 from modules.vits_model import refresh_list, init_load_model
-
+from modules.options import cmd_opts
 
 # todo: 批量处理，说话人，inline指定语言cleaner，手动输入symbol，preprocess，
 
+
 def init():
+    print(f"Launching webui with arguments: {' '.join(sys.argv[1:])}")
     if not os.path.exists("outputs"):
         os.mkdir("outputs")
     refresh_list()
@@ -16,7 +19,18 @@ def init():
 def run():
     init()
     app = create_ui()
-    app.queue(concurrency_count=3).launch(show_api=False)
+    if cmd_opts.server_name:
+        server_name = cmd_opts.server_name
+    else:
+        server_name = "0.0.0.0" if cmd_opts.listen else None
+
+    app.queue(concurrency_count=3).launch(
+        share=cmd_opts.share,
+        server_name=server_name,
+        server_port=cmd_opts.port,
+        inbrowser=cmd_opts.autolaunch,
+        show_api=False
+    )
 
 
 if __name__ == "__main__":
