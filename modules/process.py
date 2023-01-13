92e627a23f4fc1d2041559974af49c651229b1a6
Akiba <akiba@anzu.link>
2023-01-13 13:54:23 +0800

    output audio process, deprecated gr postprocess

--- modules/process.py
+++ modules/process.py
@@ -1,3 +1,7 @@
+import time
+from typing import Tuple
+import scipy.io.wavfile as wavfile
+import numpy as np
 from torch import no_grad, LongTensor
 
 import modules.vits_model as vits_model
@@ -9,10 +13,12 @@ from vits.text import text_to_sequence
 
 def text2speech(text: str, speaker, speed):
     speaker_id = vits_model.curr_vits_model.speakers.index(speaker)
-    result = process_vits(model=vits_model.curr_vits_model,
-                          text=text, speaker_id=speaker_id, speed=speed)
+    sample_rate, data = process_vits(model=vits_model.curr_vits_model,
+                                     text=text, speaker_id=speaker_id, speed=speed)
     torch_gc()
-    return result
+    save_path = f"outputs/vits/{str(int(time.time()))}.wav"
+    wavfile.write(save_path, sample_rate, data)
+    return "Success", save_path
 
 
 def text_processing(text, model: VITSModel):
@@ -28,7 +34,7 @@ def text_processing(text, model: VITSModel):
     return text_norm
 
 
-def process_vits(model: VITSModel, text: str, speaker_id, speed):
+def process_vits(model: VITSModel, text: str, speaker_id, speed) -> Tuple[int, np.array]:
     stn_tst = text_processing(text, model)
     with no_grad():
         x_tst = stn_tst.unsqueeze(0).to(device)
@@ -37,4 +43,4 @@ def process_vits(model: VITSModel, text: str, speaker_id, speed):
         audio = model.model.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=.667, noise_scale_w=0.8,
                                   length_scale=1.0 / speed)[0][0, 0].data.cpu().float().numpy()
     del stn_tst, x_tst, x_tst_lengths, sid
-    return "Success", (model.hps.data.sampling_rate, audio)
+    return model.hps.data.sampling_rate, audio
