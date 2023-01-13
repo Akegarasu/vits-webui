431ad8c2c24d2bfe5298f8868241382b48444474
Akiba <akiba@anzu.link>
2023-01-13 14:02:12 +0800

    cuda check

--- modules/devices.py
+++ modules/devices.py
@@ -2,6 +2,7 @@ import torch
 from modules import options
 
 cpu = torch.device("cpu")
+cuda_available = torch.cuda.is_available()
 
 
 def get_cuda_device():
@@ -12,16 +13,19 @@ def get_cuda_device():
 
 
 def get_optimal_device():
-    if torch.cuda.is_available():
+    if cuda_available:
         return torch.device(get_cuda_device())
     return cpu
 
 
 def torch_gc():
-    if torch.cuda.is_available():
+    if cuda_available:
         with torch.cuda.device(get_cuda_device()):
             torch.cuda.empty_cache()
             torch.cuda.ipc_collect()
 
 
 device = cpu if options.cmd_opts.cpu else get_optimal_device()
+
+if not cuda_available:
+    print("CUDA is not available, using cpu mode...")
